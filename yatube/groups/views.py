2697279400302046badb34af0ab0d8532281c9f0
Станислав Истратов <stist89@yandex.ru>
2022-06-15 14:36:46 +0300

    Добавили вьюхи

--- yatube/groups/views.py
+++ yatube/groups/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Главная страница постов')
+
+
+def group_posts(request):
+    return HttpResponse('Пост группы')
+
+
+#
